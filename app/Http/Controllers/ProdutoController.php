e697fd71261ff4cfaa173450e5bcfd810d558f25
EltonGaleti113 <egaleti2003@gmail.com>
2024-11-01 13:17:29 -0300

    'Changes about middleware sanctum'

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CriarProdutoRequest;
 use Illuminate\Http\Request;
 use App\Models\Produtos;
+use Illuminate\Database\QueryException;
+use Exception;
 
 class ProdutoController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth:sanctum');
+    }
     public function criarNovoProduto(Request $request){
-        $produto = new Produtos();
-        $produto->create($request->all());
+        try{
+            $produto = Produtos::create($request->validated());
+        }catch(Exception $e){
+            return response(
+                "Internal Server Error"
+            );
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\AuthController;
 
-Route::middleware(['sanctum'])->group(function(){
-    Route::get('/cadastrar/produtos', ProdutoController::class['criarNovoProduto']);
+Route::post('/login', AuthController::class, 'login');
+Route::post('/logout', AuthController::class, 'logout');
+
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::post('/cadastrar/produtos', [ProdutoController::class, 'criarNovoProduto']);
 });
