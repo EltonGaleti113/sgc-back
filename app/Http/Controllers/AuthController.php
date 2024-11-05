664bdeb6bcb012f511a1a89c05f88765f858f64f
elton.galeti <elton.galeti@grupoagtech.com>
2024-11-04 22:19:00 -0300

    'middleware created, login method working'

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,46 +1,53 @@
 <?php
-
 namespace App\Http\Controllers;
 
-use Exception;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
-use Tymon\JWTAuth\Facades\JWTAuth;
-use Tymon\JWTAuth\Exceptions\JWTException;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    /**
-     * Create new AuthContrller Instance
-     * 
-     * @return void
-     **/
-    public function __construct()
+    public function register(Request $request)
     {
-        $this->middleware('auth:api', ['except'=>['login']]);
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['message' => 'User registered successfully', 'user' => $user], 201);
     }
 
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
 
-        try {
-            if (!$token=JWTAuth::attempt($credentials)) {
-                return response->json(['error'=>'Credenciais Inválidas'], 401);
-            }
-        } catch (JWTException $e) {
-            return response()->json(['error'=>'Erro ao criar token'], 500);
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('authToken')->plainTextToken;
+
+            return response()->json(['token' => $token, 'user' => $user], 200);
         }
-        return response()->json(compact('token'));
+
+        return response()->json(['error' => 'Unauthorized'], 401);
     }
 
     public function logout()
     {
-        try{
-            JWTAuth::invalidate(JWTAuth::getToken());
-            return response()->json(['mensagem'=>'desconectado com sucesso']);
-        }catch(JWTException $e){
-            return response()->json(['error'=>'falha ao desconectar'], 500);
-        }
+        Auth::user()->tokens()->delete();
+
+        return response()->json(['message' => 'Logged out successfully'], 200);
     }
 }
